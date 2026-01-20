b2c4a51d97e23c23073f16716dcfe316922c333c
Neeschal1 <neeschalpok04@gmail.com>
2026-01-20 20:26:47 +0545

    pinecone vector db modified and updated with an unique username for each product

--- ai/embeddings.py
+++ ai/embeddings.py
@@ -4,10 +4,29 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from celery import shared_task
 from .pinecone import pinecone_setup
 from django.core.cache import cache
+import random
+import string
 
 # Model import and initialization
 _embedding_model = None
 
+
+# For unique temp id in every new attempt
+def unique_id():
+    numbers = string.digits
+    words = string.ascii_letters
+    new_id = ""
+    length = 10
+    for _ in range(length):
+        random_number = random.choice(numbers)
+        random_word = random.choice(words)
+        new_id = new_id + random_number + random_word
+        if len(new_id) == length:
+            break
+    return new_id
+
+
+# Model setup
 def get_embedding_model():
     global _embedding_model
     if _embedding_model is None:
@@ -17,17 +36,17 @@ def get_embedding_model():
 
 @shared_task
 def initialize_embeddings(username, productname, productcategory, productdescription):
-    
     embedding = get_embedding_model()
     vectors = embedding.embed_query(productdescription)
-    print(vectors)
     
     # Temporary id
-    temp_id = f"temp_{username}"
+    unique_number = unique_id()
+    temp_id = f"temp_{username}_{unique_number}"
+    
+    print(vectors)
+    print(temp_id)
     
     # Store in pinecone
     db_data = pinecone_setup(temp_id, vectors, productname, productcategory, username)
-    
     print(db_data)
-    
     return db_data
--- ai/pinecone.py
+++ ai/pinecone.py
@@ -4,17 +4,26 @@ from env_config import Config
 pc = Pinecone(api_key=Config.PINECONE_API_KEY)
 index = pc.Index(name="dafyecommerce")
 
+
 def pinecone_setup(tempid, vectors, productname, productcategory, username):
     db_details = index.upsert(
-        vectors=[{
-            "id": tempid,
-            "values": vectors,
-            "metadata": {
-                "product_name": productname,
-                "product_category": productcategory,
-                "username": username,
-                "status": "pending"
-            }}]
+        vectors=[
+            {
+                "id": tempid,
+                "values": vectors,
+                "metadata": {
+                    "product_name": productname,
+                    "product_category": productcategory,
+                    "username": username,
+                    "status": "pending",
+                },
+            }
+        ]
     )
-    
-    return db_details
+
+    return {"status": "success", "vector_id": tempid}
+
+
+def fetch_data_from_pinecone():
+    data = index.fetch()
+    return
