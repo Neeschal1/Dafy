cf937d2ee501c47b30b4d728bf3e3d6d0bd49e35
Neeschal1 <neeschalpok04@gmail.com>
2026-01-20 20:44:52 +0545

    implemented username logic in product's images detail

--- apps/products/services/images.py
+++ apps/products/services/images.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from django.core.cache import cache
+from django.contrib.auth.models import User
 
 def product_images(image):
     cache_key = image.validated_data['Username']
@@ -9,10 +10,18 @@ def product_images(image):
     imagefour = image.validated_data['Image_four']
     imagefive = image.validated_data['Image_five']
     
-    details = [imageone, imagetwo, imagethree, imagefour, imagefive]
-    cache.set(f"images_{cache_key}", details, timeout=2000)
+    try:
+        user = User.objects.get(username = cache_key)
+    except User.DoesNotExist:
+        return Response({"Message":f"User with the username: {cache_key} does not exists. Please signup and account first and then try again!"})
     
-    return Response({"Response":{"Details":{
-        "Initial Details":cache.get(cache_key),
-        "Images Details": cache.get(f"images_{cache_key}")
-    }}})
+    if user:
+        details = [imageone, imagetwo, imagethree, imagefour, imagefive]
+        cache.set(f"images_{cache_key}", details, timeout=2000)
+        
+        return Response({"Response":{"Details":{
+            "Initial Details":cache.get(cache_key),
+            "Images Details": cache.get(f"images_{cache_key}")
+        }}})
+    else:
+        return Response({"Message":"Something error occred. Might be you don't have a stable network connection."})
