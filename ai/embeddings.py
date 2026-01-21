062bd1f7c567bd6f298586ee38b98d70138c9261
Neeschal1 <neeschalpok04@gmail.com>
2026-01-21 12:51:12 +0545

    included worker_process_init decorator to let the model load once, when the redis server is loading

--- ai/embeddings.py
+++ ai/embeddings.py
@@ -2,14 +2,17 @@ from pinecone import Pinecone
 from env_config import Config
 from langchain_huggingface import HuggingFaceEmbeddings 
 from celery import shared_task
+from celery.signals import worker_process_init
 from .vector.operations.upsert import pinecone_setup
-from django.core.cache import cache
-import random
-import string
 
 # Model import and initialization
 _embedding_model = None
 
+@worker_process_init.connect
+def init_worker_process(sender=None, **kwargs):
+    global _embedding_model
+    _embedding_model = HuggingFaceEmbeddings(model_name=Config.EMBEDDINGS_MODEL_NAME)
+    print("Model loaded on worker startup!")
 
 # Model setup
 def get_embedding_model():
@@ -18,19 +21,11 @@ def get_embedding_model():
         _embedding_model = HuggingFaceEmbeddings(model_name=Config.EMBEDDINGS_MODEL_NAME)
     return _embedding_model
 
-
 @shared_task
 def initialize_embeddings(username, productname, productcategory, productdescription, temp_id):
     embedding = get_embedding_model()
     vectors = embedding.embed_query(productdescription)
     
-    # Generate a temporary unique id
-    temp_id = f"temp_{username}_{temp_id}"
-    
-    print(vectors)
-    print(temp_id)
-    
     # Store in pinecone
     db_data = pinecone_setup(temp_id, vectors, productname, productcategory, username)
-    print(db_data)
     return db_data
