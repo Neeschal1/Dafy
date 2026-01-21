7f22c1c9554b86f1950d3024ef489e5bc0f6fdff
Neeschal1 <neeschalpok04@gmail.com>
2026-01-21 20:09:17 +0545

    similar data fetching logic implemented.

--- ai/semantic_search.py
+++ ai/semantic_search.py
@@ -4,19 +4,18 @@ from apps.products.models.entities import Product
 from rest_framework.response import Response
 from env_config import Config
 
-embedding = 1
-
-
 def selected_product_for_similarity(selected, other):
     # For appending similar product's indexes and similar description's data
     indexed_details = []
-    similar_products_description = []
+    similar_products_ids = []
     embedded_data = []
+    product_ids = []
     
-    # try:
-    #     products = Product.objects.values_list("Product_Description", flat=True)
-    # except Product.DoesNotExist:
-    #     return Response({"Message":"Product doesnot exists. Please try again later!"})
+    try:
+        products = Product.objects.values_list("Product_Description", flat=True)
+    except Product.DoesNotExist:
+        return Response({"Message":"Product doesnot exists. Please try again later!"})
+    print(f"\n\n\n\n\n\nOther data: {other}\n\n\n\n\n\n")
     
     # Embeddings of both, particular description and whole product's description
     # whole_data_query = embedding.embed_documents(products)
@@ -25,23 +24,39 @@ def selected_product_for_similarity(selected, other):
         
     for data in other:
         embedded_data.append(data['Vector'])
+        product_ids.append(data['Product_id'])
     print(f"Embedded Data: \n{embedded_data}\n\n\n\n")
+    print(f"Product Id's: \n{product_ids}\n\n\n\n")
     print("Working perfectly!\n\n\n\n\n\n\n\n\n\n\n")
     
     # Cosine similarity and indexing
     data = cosine_similarity([selected], embedded_data)[0]
     print(f"Data: {data}\n\n\n\n\n\n\n\n")
-    for index, detail in enumerate(data):
-        indexed_details.append((index, detail))
+    
+    
+    
+    # for index, detail in enumerate(data):
+    #     indexed_details.append((index, detail))
+    
+    similarity_result = list(zip(product_ids, data))
+    for i in similarity_result:
+        print(i)
+        
+    top_data = [(prod_id, score) for prod_id, score in similarity_result if score >= 0.1]
+    print(f"\n\n\n\n\n{top_data}\n\n\n\n\n")
         
     # Sorting each of them
-    sorted_data = sorted(indexed_details, key=lambda i: i[1], reverse=True)[1:5]
+    sorted_data = sorted(top_data, key=lambda i: i[1], reverse=True)[1:5]
     
+    for index in sorted_data:
+        similar_products_ids.append(index[0])
     # Similar Products
     # for index in sorted_data:
         # similar_products_description.append()
         # similar_products_description.append(products[index[0]])
         
     print(f"\n\n\n\n\nSorted_data: {sorted_data}\n\n\n\n\n")
+    print(f"Products from database: {products}\n\n\n\n\n\n\n\n")
+    print(f"Product ID's of similar products: {similar_products_ids}\n\n\n\n\n\n\n\n")
     
-    return 
+    return similar_products_ids
